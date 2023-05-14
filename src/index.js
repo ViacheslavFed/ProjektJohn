803bcf446bd8bb27226ae3b4867e81d0d6b51290
Viacheslav Fedorenko <vf769829@gmail.com>
2023-05-14 16:07:20 +0200

    Homwork

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <BrowserRouter>
-          <App />
-    </BrowserRouter>
-
-
-);
+const router = createHashRouter([
+    {
+      path: '/*',
+      element: <App />
+    }
+  ])
+  
+  
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  );
 
 
